bfb52fa937bb2e5f331f32463e50a259928d9d15
webee.yw <webee.yw@gmail.com>
2020-08-08 15:32:55 +0800

    fix arrow

--- src/components/Sudoku/Board/Arrow.js
+++ src/components/Sudoku/Board/Arrow.js
@@ -3,20 +3,22 @@ import styles from './Arrow.module.scss';
 import styled from 'styled-components';
 import * as positions from '../../../libs/position';
 
+// shortend arrow length, to avoid tail-head contact.
+const dd = 0.03;
+// percentage for a cell
+const pc = (1 - 1.5 / 100) / 9;
+
 const Arrow = styled.div`
   user-select: none;
   pointer-events: none;
   position: absolute;
-  border-top: 2px ${({ type }) => type} #ccff00;
+  border-top: 2px ${({ type }) => type} red;
   width: ${({ distance }) => distance * 100}%;
   height: ${({ distance }) => distance * 100}%;
   transform-origin: top left;
   transform: translate(${({ x, distance }) => (x / distance) * 100}%, ${({ y, distance }) => (y / distance) * 100}%)
-    rotate(${({ deg }) => deg}deg);
+    rotate(${({ deg }) => deg}deg) translate(${({ distance }) => (dd / 2 / distance) * 100}%);
 `;
-
-// percentage for a cell
-const pc = (1 - 1.5 / 100) / 9;
 
 const getCoord = (pos, d) => {
   const { row, col } = pos;
@@ -49,7 +51,7 @@ export default ({ type = 'solid', startPos, startDigit, endPos, endDigit }) => {
   console.log(type);
 
   return (
-    <Arrow x={sc[0]} y={sc[1]} distance={distance} deg={deg} type={type}>
+    <Arrow x={sc[0]} y={sc[1]} distance={distance - dd} deg={deg} type={type}>
       <div className={styles.Pointer}></div>
     </Arrow>
   );
